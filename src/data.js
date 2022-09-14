5e77e2b7ee6c07ba6cbdd124ab97ccd4e17cd86f
Baiju Abraham <baijuka@yahoo.com>
2022-09-14 16:04:47 +0100

    Add style to components

--- src/data.js
+++ src/data.js
@@ -39,3 +39,38 @@ export const categories = [
         title: 'CHILDRENS WEAR'
     },
 ]
+
+export const popularProducts = [
+    {
+        id:1,
+        img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+      },
+      {
+        id:2,
+        img:"https://burst.shopifycdn.com/photos/torso-of-a-person-wearing-white-shirt-product-mockup.jpg?width=925&format=pjpg&exif=1&iptc=1",
+      },
+      {
+        id:3,
+        img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+      },
+      {
+        id:4,
+        img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+      },
+      {
+        id:5,
+        img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+      },
+      {
+        id:6,
+        img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+      },
+      {
+        id:7,
+        img:"https://uk.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-colour-blossom-bb-star-ear-studs-pink-gold-pink-mother-of-pearl-and-diamonds--Q96667_PM2_Front%20view.jpg",
+      },
+      {
+        id:8,
+        img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+      },
+]
